d4e6a89661a3919b1a054f128902e9f948e1dfdf
trenfranco <trenfranco@gmail.com>
2022-07-19 15:31:51 -0700

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new)
 		return (NULL);
 
+	new->left = NULL;
+	new->right = NULL;
 	new->n = value;
 	new->parent = parent;
 	return (new);
