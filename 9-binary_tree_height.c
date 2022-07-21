0f32ec33275a0fff020da00f0020b4e9e6c635c1
trenfranco <trenfranco@gmail.com>
2022-07-20 18:18:51 -0700

    ex 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures tree height
+ * @tree: tree
+ * Return: height or 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t i = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->left || tree->right)
+	{
+		if (!tree->left)
+		{
+			tree = tree->right;
+			i++;
+			continue;
+		}
+		tree = tree->left;
+		i++;
+	}
+	return (i);
+}
