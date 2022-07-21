828360608b7625daeaf963876b00681981d6e9e6
trenfranco <trenfranco@gmail.com>
2022-07-20 18:25:03 -0700

    ex 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	
+	if (!tree->parent && !tree->left && !tree->right)
+		return (1);
 
 	while (tree->left || tree->right)
 	{
